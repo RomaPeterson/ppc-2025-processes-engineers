3734a87ec447b1a2fce6e318e5439f484116c4ce
romaplomba <144802312+RomaPeterson@users.noreply.github.com>
2025-11-20 01:08:42 +0300

    Update main.cpp

--- tasks/tasks/peterson_r_min_val_matrix/tests/performance/main.cpp
+++ tasks/tasks/peterson_r_min_val_matrix/tests/performance/main.cpp
@@ -9,33 +9,30 @@ namespace peterson_r_min_val_matrix {
 
 class PetersonRunPerfTest : public ppc::util::BaseRunPerfTests<InType, OutType> {
  protected:
-  void SetUp() override {
-    input_data_ = 100;
-  }
+  void SetUp() override { input_data_ = 100; }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return !output_data.empty() && output_data.size() == static_cast<size_t>(input_data_);
+    return !output_data.empty() &&
+           output_data.size() == static_cast<size_t>(input_data_);
   }
 
-  InType GetTestInputData() final {
-    return input_data_;
-  }
+  InType GetTestInputData() final { return input_data_; }
 
  private:
   InType input_data_ = 0;
 };
 
-TEST_P(PetersonRunPerfTest, RunPerfModes) {
-  ExecuteTest(GetParam());
-}
+TEST_P(PetersonRunPerfTest, RunPerfModes) { ExecuteTest(GetParam()); }
 
-const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, PetersonRMinValMatrixMPI, PetersonRMinValMatrixSEQ>(PPC_SETTINGS_peterson_r_min_val_matrix);
+const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<
+    InType, PetersonRMinValMatrixMPI, PetersonRMinValMatrixSEQ>(
+    PPC_SETTINGS_peterson_r_min_val_matrix);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
 const auto kPerfTestName = PetersonRunPerfTest::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, PetersonRunPerfTest, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, PetersonRunPerfTest, kGtestValues,
+                         kPerfTestName);
 
 }  // namespace peterson_r_min_val_matrix
