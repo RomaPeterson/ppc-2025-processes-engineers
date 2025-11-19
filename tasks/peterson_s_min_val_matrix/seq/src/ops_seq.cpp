f17bb82c7b01f57ecf1cdc47b476b2f287dbe4b8
romaplomba <144802312+RomaPeterson@users.noreply.github.com>
2025-11-19 23:34:37 +0300

    Create ops_seq.cpp

---
+++ tasks/peterson_s_min_val_matrix/seq/src/ops_seq.cpp
@@ -0,0 +1,49 @@
+#include "peterson_s_min_val_matrix/seq/include/ops_seq.hpp"
+
+#include <algorithm>
+#include <vector>
+
+#include "peterson_s_min_val_matrix/common/include/common.hpp"
+
+namespace peterson_s_min_val_matrix {
+
+PetersonSMinValMatrixSEQ::PetersonSMinValMatrixSEQ(const InType &in) {
+  SetTypeOfTask(GetStaticTypeOfTask());
+  GetInput() = in;
+  GetOutput().clear();
+}
+
+bool PetersonSMinValMatrixSEQ::ValidationImpl() {
+  return (GetInput() > 0) && (GetOutput().empty());
+}
+
+bool PetersonSMinValMatrixSEQ::PreProcessingImpl() {
+  GetOutput().clear();
+  GetOutput().reserve(GetInput());
+  return true;
+}
+
+bool PetersonSMinValMatrixSEQ::RunImpl() {
+  InType n = GetInput();
+  if (n == 0) return false;
+
+  GetOutput().clear();
+  GetOutput().reserve(n);
+
+  for (InType j = 0; j < n; ++j) {
+    InType min_val = j + 1;
+    for (InType i = 1; i < n; ++i) {
+      InType current_val = i * n + j + 1;
+      min_val = std::min(min_val, current_val);
+    }
+    GetOutput().push_back(min_val);
+  }
+
+  return true;
+}
+
+bool PetersonSMinValMatrixSEQ::PostProcessingImpl() {
+  return !GetOutput().empty();
+}
+
+}  // namespace peterson_s_min_val_matrix
