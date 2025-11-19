ceeb81c70351ce8ede35e41a4d512ad56f0a08a7
romaplomba <144802312+RomaPeterson@users.noreply.github.com>
2025-11-20 00:59:18 +0300

    Create main.cpp

---
+++ tasks/tasks/peterson_r_min_val_matrix/performance/main.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+
+#include "peterson_r_min_val_matrix/common/include/common.hpp"
+#include "peterson_r_min_val_matrix/mpi/include/ops_mpi.hpp"
+#include "peterson_r_min_val_matrix/seq/include/ops_seq.hpp"
+#include "util/include/perf_test_util.hpp"
+
+namespace peterson_r_min_val_matrix {
+
+class PetersonRunPerfTest : public ppc::util::BaseRunPerfTests<InType, OutType> {
+ protected:
+  void SetUp() override {
+    input_data_ = 100;  // Reasonable size for performance testing
+  }
+
+  bool CheckTestOutputData(OutType &output_data) final {
+    return !output_data.empty() && output_data.size() == static_cast<size_t>(input_data_);
+  }
+
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+
+ private:
+  InType input_data_ = 0;
+};
+
+TEST_P(PetersonRunPerfTest, RunPerfModes) {
+  ExecuteTest(GetParam());
+}
+
+const auto kAllPerfTasks =
+    ppc::util::MakeAllPerfTasks<InType, PetersonRMinValMatrixMPI, PetersonRMinValMatrixSEQ>(PPC_SETTINGS_peterson_r_min_val_matrix);
+
+const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+
+const auto kPerfTestName = PetersonRunPerfTest::CustomPerfTestName;
+
+INSTANTIATE_TEST_SUITE_P(RunModeTests, PetersonRunPerfTest, kGtestValues, kPerfTestName);
+
+}  // namespace peterson_r_min_val_matrix
