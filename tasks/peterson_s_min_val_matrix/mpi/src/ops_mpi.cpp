a2b6f557e4160ca9cec6a74940608c8d0a2ac3fe
romaplomba <144802312+RomaPeterson@users.noreply.github.com>
2025-11-19 23:48:25 +0300

    Update ops_mpi.cpp

--- tasks/peterson_s_min_val_matrix/mpi/src/ops_mpi.cpp
+++ tasks/peterson_s_min_val_matrix/mpi/src/ops_mpi.cpp
@@ -1,6 +1,7 @@
 #include "peterson_s_min_val_matrix/mpi/include/ops_mpi.hpp"
 
 #include <mpi.h>
+
 #include <algorithm>
 #include <vector>
 
@@ -15,63 +16,86 @@ PetersonSMinValMatrixMPI::PetersonSMinValMatrixMPI(const InType &in) {
 }
 
 bool PetersonSMinValMatrixMPI::ValidationImpl() {
-  return (GetInput() > 0) && (GetOutput().empty());
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  if (rank == 0) {
+    return GetOutput().empty();
+  }
+  return true;
 }
 
 bool PetersonSMinValMatrixMPI::PreProcessingImpl() {
-  GetOutput().clear();
-  GetOutput().reserve(GetInput());
+  int rank = 0;
+  int size = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  if (rank == 0) {
+    GetOutput().clear();
+  }
   return true;
 }
 
 bool PetersonSMinValMatrixMPI::RunImpl() {
-  InType n = GetInput();
-  if (n == 0) return false;
-
-  int rank, size;
+  int rank = 0;
+  int size = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  int cols_per_proc = n / size;
-  int remainder = n % size;
-  int start_col = rank * cols_per_proc + (rank < remainder ? rank : remainder);
-  int num_local_cols = cols_per_proc + (rank < remainder ? 1 : 0);
-
-  std::vector<InType> local_mins(num_local_cols);
-
-  for (int j = 0; j < num_local_cols; ++j) {
-    int col_idx = start_col + j;
-    InType min_val = col_idx + 1;
-    for (int i = 1; i < n; ++i) {
-      InType val = i * n + col_idx + 1;
-      if (val < min_val) min_val = val;
-    }
-    local_mins[j] = min_val;
-  }
-
-  std::vector<int> recvcounts(size), displs(size);
-  for (int i = 0; i < size; ++i) {
-    recvcounts[i] = cols_per_proc + (i < remainder ? 1 : 0);
-    displs[i] = i * cols_per_proc + (i < remainder ? i : remainder);
-  }
-
-  GetOutput().resize(n);
-
-  MPI_Gatherv(local_mins.data(), num_local_cols, MPI_INT,
-              rank == 0 ? GetOutput().data() : nullptr, recvcounts.data(),
-              displs.data(), MPI_INT, 0, MPI_COMM_WORLD);
-
+  int n = 0;
   if (rank == 0) {
-    MPI_Bcast(GetOutput().data(), n, MPI_INT, 0, MPI_COMM_WORLD);
-  } else {
-    MPI_Bcast(GetOutput().data(), n, MPI_INT, 0, MPI_COMM_WORLD);
+    n = GetInput();
   }
 
+  MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+  if (n == 0) {
+    if (rank == 0) {
+      GetOutput().clear();
+    }
+    return true;
+  }
+
+  int chunk_size = n / size;
+  int remainder = n % size;
+
+  int start_col = (rank * chunk_size) + std::min(rank, remainder);
+  int end_col = start_col + chunk_size + (rank < remainder ? 1 : 0);
+
+  std::vector<int> local_mins;
+  for (int j = start_col; j < end_col; ++j) {
+    int min_val = j + 1;
+    for (int i = 1; i < n; ++i) {
+      int current_val = (i * n) + j + 1;
+      if (current_val < min_val) {
+        min_val = current_val;
+      }
+    }
+    local_mins.push_back(min_val);
+  }
+
+  std::vector<int> recvcounts(size);
+  std::vector<int> displs(size);
+  for (int i = 0; i < size; ++i) {
+    int proc_cols = chunk_size + (i < remainder ? 1 : 0);
+    recvcounts[i] = proc_cols;
+    displs[i] = (i * chunk_size) + std::min(i, remainder);
+  }
+
+  std::vector<int> global_mins(n);
+  MPI_Gatherv(local_mins.data(), static_cast<int>(local_mins.size()), MPI_INT,
+              global_mins.data(), recvcounts.data(), displs.data(), MPI_INT, 0,
+              MPI_COMM_WORLD);
+
+  MPI_Bcast(global_mins.data(), n, MPI_INT, 0, MPI_COMM_WORLD);
+
+  GetOutput() = global_mins;
   return true;
 }
 
 bool PetersonSMinValMatrixMPI::PostProcessingImpl() {
-  return !GetOutput().empty();
+  return true;
 }
 
 }  // namespace peterson_s_min_val_matrix
