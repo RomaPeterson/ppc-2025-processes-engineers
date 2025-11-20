221f48fd1ee99949c473cda4800afd674ae2785e
romaplomba <144802312+RomaPeterson@users.noreply.github.com>
2025-11-20 16:44:49 +0300

    Create common.hpp

---
+++ tasks/peterson_r_min_val_matrix/common/include/common.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include "task/include/task.hpp"
+
+namespace peterson_r_min_val_matrix {
+
+using InType = int;
+using OutType = std::vector<int>;
+using TestType = std::tuple<int, std::string>;
+using BaseTask = ppc::task::Task<InType, OutType>;
+
+}  // namespace peterson_r_min_val_matrix
