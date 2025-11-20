753a5fddb16be513aa083a73b94431645deee4d2
romaplomba <144802312+RomaPeterson@users.noreply.github.com>
2025-11-20 17:54:32 +0300

    Update common.hpp

--- tasks/peterson_r_min_val_matrix/common/include/common.hpp
+++ tasks/peterson_r_min_val_matrix/common/include/common.hpp
@@ -13,4 +13,7 @@ using OutType = std::vector<int>;
 using TestType = std::tuple<int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
+constexpr int kMaxMatrixSize = 10000;
+constexpr int kMinMatrixSize = 1;
+
 }  // namespace peterson_r_min_val_matrix
