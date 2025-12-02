758d4119ef858993c87138cc2e00d97aff060e99
romaplomba <144802312+RomaPeterson@users.noreply.github.com>
2025-12-02 18:56:32 +0300

    Update main.cpp

--- tasks/peterson_r_min_matrix_cols_elm/tests/performance/main.cpp
+++ tasks/peterson_r_min_matrix_cols_elm/tests/performance/main.cpp
@@ -13,67 +13,67 @@
 
 namespace peterson_r_min_matrix_cols_elm {
 
-    class PetersonRMinMatrixColsElmPerfTest : public ppc::util::BaseRunPerfTests<InType, OutType> {
-        std::vector<int> correct_test_output_data_;
-        InType input_data_;
+class PetersonRMinMatrixColsElmPerfTest : public ppc::util::BaseRunPerfTests<InType, OutType> {
+  std::vector<int> correct_test_output_data_;
+  InType input_data_;
 
-        void SetUp() override {
-            Generate(10000, 10000, 123);
+  void SetUp() override {
+    Generate(10000, 10000, 123);
+  }
+
+  bool CheckTestOutputData(OutType &output_data) final {
+    for (std::size_t i = 0; i < correct_test_output_data_.size(); i++) {
+      if (output_data[i] != correct_test_output_data_[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+
+  void Generate(std::size_t m, std::size_t n, int seed) {
+    std::mt19937 gen(seed);
+    std::uniform_int_distribution<> idis(-10, 20);
+
+    std::vector<int> val(m * n);
+    std::vector<int> answer(n);
+    for (std::size_t i = 0; i < n; i++) {
+      val[i] = idis(gen);
+      answer[i] = val[i];
+    }
+
+    for (std::size_t i = 1; i < m; i++) {
+      for (std::size_t j = 0; j < n; j++) {
+        val[i * n + j] = idis(gen);
+        if (answer[j] > val[i * n + j]) {
+          answer[j] = std::min(answer[j], val[i * n + j]);
         }
+      }
+    }
+    input_data_ = std::make_tuple(m, n, val);
+    correct_test_output_data_ = answer;
+  }
+};
 
-        bool CheckTestOutputData(OutType& output_data) final {
-            for (std::size_t i = 0; i < correct_test_output_data_.size(); i++) {
-                if (output_data[i] != correct_test_output_data_[i]) {
-                    return false;
-                }
-            }
-            return true;
-        }
+namespace {
 
-        InType GetTestInputData() final {
-            return input_data_;
-        }
+TEST_P(PetersonRMinMatrixColsElmPerfTest, RunPerfModes) {
+  ExecuteTest(GetParam());
+}
 
-        void Generate(std::size_t m, std::size_t n, int seed) {
-            std::mt19937 gen(seed);
-            std::uniform_int_distribution<> idis(-10, 20);
+const auto kAllPerfTasks =
+    ppc::util::MakeAllPerfTasks<InType, PetersonRMinMatrixColsElmMPI, PetersonRMinMatrixColsElmSEQ>(
+        PPC_SETTINGS_peterson_r_min_matrix_cols_elm);
 
-            std::vector<int> val(m * n);
-            std::vector<int> answer(n);
-            for (std::size_t i = 0; i < n; i++) {
-                val[i] = idis(gen);
-                answer[i] = val[i];
-            }
+const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-            for (std::size_t i = 1; i < m; i++) {
-                for (std::size_t j = 0; j < n; j++) {
-                    val[i * n + j] = idis(gen);
-                    if (answer[j] > val[i * n + j]) {
-                        answer[j] = std::min(answer[j], val[i * n + j]);
-                    }
-                }
-            }
-            input_data_ = std::make_tuple(m, n, val);
-            correct_test_output_data_ = answer;
-        }
-    };
+const auto kPerfTestName = PetersonRMinMatrixColsElmPerfTest::CustomPerfTestName;
 
-    namespace {
+INSTANTIATE_TEST_SUITE_P(RunModeTests, PetersonRMinMatrixColsElmPerfTest, kGtestValues, kPerfTestName);
 
-        TEST_P(PetersonRMinMatrixColsElmPerfTest, RunPerfModes) {
-            ExecuteTest(GetParam());
-        }
-
-        const auto kAllPerfTasks =
-            ppc::util::MakeAllPerfTasks<InType, PetersonRMinMatrixColsElmMPI, PetersonRMinMatrixColsElmSEQ>(
-                PPC_SETTINGS_peterson_r_min_matrix_cols_elm);
-
-        const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
-
-        const auto kPerfTestName = PetersonRMinMatrixColsElmPerfTest::CustomPerfTestName;
-
-        INSTANTIATE_TEST_SUITE_P(RunModeTests, PetersonRMinMatrixColsElmPerfTest, kGtestValues, kPerfTestName);
-
-    }  // namespace
+}  // namespace
 
 }  // namespace peterson_r_min_matrix_cols_elm
