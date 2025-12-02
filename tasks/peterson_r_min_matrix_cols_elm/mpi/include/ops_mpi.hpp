59551100970dc1cdbcbbd3bd09d23074a9017d9f
romaplomba <144802312+RomaPeterson@users.noreply.github.com>
2025-12-02 18:55:14 +0300

    Update ops_mpi.hpp

--- tasks/peterson_r_min_matrix_cols_elm/mpi/include/ops_mpi.hpp
+++ tasks/peterson_r_min_matrix_cols_elm/mpi/include/ops_mpi.hpp
@@ -1,27 +1,27 @@
 #pragma once
 
-#include <vector>    
+#include <vector>
 
 #include "peterson_r_min_matrix_cols_elm/common/include/common.hpp"
 #include "task/include/task.hpp"
 
 namespace peterson_r_min_matrix_cols_elm {
 
-    class PetersonRMinMatrixColsElmMPI : public BaseTask {
-    public:
-        static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
-            return ppc::task::TypeOfTask::kMPI;
-        }
-        explicit PetersonRMinMatrixColsElmMPI(const InType& in);
+class PetersonRMinMatrixColsElmMPI : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kMPI;
+  }
+  explicit PetersonRMinMatrixColsElmMPI(const InType &in);
 
-    private:
-        bool ValidationImpl() override;
-        bool PreProcessingImpl() override;
-        bool RunImpl() override;
-        bool PostProcessingImpl() override;
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
 
-        std::vector<int> t_matrix_;
-        bool valid_ = false;
-    };
+  std::vector<int> t_matrix_;
+  bool valid_ = false;
+};
 
 }  // namespace peterson_r_min_matrix_cols_elm
