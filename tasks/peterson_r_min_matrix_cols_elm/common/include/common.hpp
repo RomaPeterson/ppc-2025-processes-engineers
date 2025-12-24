1b0644d157d4a28aafa1f7f1c7047d2a72b8fedf
romaplomba <144802312+RomaPeterson@users.noreply.github.com>
2025-12-24 22:55:46 +0300

    Add files via upload

--- tasks/peterson_r_min_matrix_cols_elm/common/include/common.hpp
+++ tasks/peterson_r_min_matrix_cols_elm/common/include/common.hpp
@@ -1,17 +1,16 @@
 #pragma once
 
-#include <cstddef>
 #include <string>
 #include <tuple>
 #include <vector>
 
 #include "task/include/task.hpp"
 
-namespace peterson_r_min_matrix_cols_elm {
+namespace peterson_r_matrix_min_by_columns {
 
-using InType = std::tuple<std::size_t, std::size_t, std::vector<int>>;
+using InType = int;
 using OutType = std::vector<int>;
-using TestType = std::tuple<std::string, std::string, std::vector<int>>;
+using TestType = std::tuple<int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
-} // namespace peterson_r_min_matrix_cols_elm
+}  // namespace peterson_r_matrix_min_by_columns
--- tasks/peterson_r_min_matrix_cols_elm/mpi/include/ops_mpi.hpp
+++ tasks/peterson_r_min_matrix_cols_elm/mpi/include/ops_mpi.hpp
@@ -1,24 +1,22 @@
 #pragma once
 
-#include "peterson_r_min_matrix_cols_elm/common/include/common.hpp"
+#include "peterson_r_matrix_min_by_columns/common/include/common.hpp"
 #include "task/include/task.hpp"
 
-namespace peterson_r_min_matrix_cols_elm {
+namespace peterson_r_matrix_min_by_columns {
 
-class PetersonRMinMatrixColsElmMPI : public BaseTask {
-public:
+class PetersonRMinMatrixMPI : public BaseTask {
+ public:
   static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
     return ppc::task::TypeOfTask::kMPI;
   }
-  explicit PetersonRMinMatrixColsElmMPI(const InType &in);
+  explicit PetersonRMinMatrixMPI(const InType &in);
 
-private:
+ private:
   bool ValidationImpl() override;
   bool PreProcessingImpl() override;
   bool RunImpl() override;
   bool PostProcessingImpl() override;
-
-  bool valid_ = false;
 };
 
-} // namespace peterson_r_min_matrix_cols_elm
+}  // namespace peterson_r_matrix_min_by_columns
