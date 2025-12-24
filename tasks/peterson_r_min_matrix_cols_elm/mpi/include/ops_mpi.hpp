9f8d891f6a36bebd99d331a69f4e2a758ab9328a
romaplomba <144802312+RomaPeterson@users.noreply.github.com>
2025-12-24 23:29:37 +0300

    Add files via upload

---
+++ tasks/peterson_r_min_matrix_cols_elm/mpi/include/ops_mpi.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "peterson_r_min_matrix_cols_elm/common/include/common.hpp"
+#include "task/include/task.hpp"
+
+namespace peterson_r_matrix_min_by_columns {
+
+class PetersonRMinMatrixMPI : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kMPI;
+  }
+  explicit PetersonRMinMatrixMPI(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+};
+
+}  // namespace peterson_r_matrix_min_by_columns
